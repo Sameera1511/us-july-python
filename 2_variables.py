f776658cb504aef1e69eb5c8c2d52a961ccc7ec8
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-07-13 20:36:03 +0530

    completed variables, numeric datatypes

---
+++ 2_variables.py
@@ -0,0 +1,67 @@
+# Variables:
+    # Variables are nothing but references for data.
+    # Variables are like containers which stores data.
+
+# Rules to create variable in python:
+    # can use alphabets (a-z, A-Z) to create variable.
+    # can use numbers (0-9) to create variable but do not start variable with number.
+        # Example
+                # 2people = "Men"   --> Wrong
+                # people2 = "Men"   --> correct
+                # p2e2455ople = "men" --> correct
+
+    # can not use any special charecters except _
+        # Example
+                # @friends = "True Life" --> Wrong
+                # my_friend = "Sania"  --> Correct
+
+    # can not use any keywords
+        # Keywords are those words which are used by python 
+        # internally.
+
+# import keyword
+# print(keyword.kwlist)
+
+    # 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 
+    # 'await', 'break', 'class', 'continue', 'def', 'del',
+    # 'elif', 'else', 'except', 'finally', 'for', 'from', 
+    # 'global', 'if', 'import', 'in', 'is', 'lambda', 
+    # 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 
+    # 'try', 'while', 'with', 'yield'
+
+
+# False = "Hello"
+# print(False)
+
+# yield = 23 
+
+    # Always create meaningful varible.
+
+# ss = 23
+# mg = "Ash Patel"
+
+# age = 23
+# manager = "Ash Patel"
+
+
+#-----------------------------------------
+
+
+
+# DEMO = "python"  
+# print(DEMO)
+
+# 3.4littre = "Oil" 
+# lit3.4re = "Gas"
+# lit3re = "Disel" 
+# print(lit3re)
+
+
+# continue = "David" 
+
+# Name = "Samista"
+# name = "Molla"
+# NAME = "Sai"
+# NaMe = "David"
+
+# print(NAme)
---
+++ 3_datatypes.py
@@ -0,0 +1,52 @@
+# Datatpes:
+    # 1) Int => Integer
+        # Integers are numbers without decimal point including 0
+        # Integers can be +ve , -ve
+
+# x = 6
+# print(x)
+# print(type(x))   #<class 'int'>
+
+# y = 0
+# print(y)
+# print(type(y))
+
+# z = 4.0
+# print(z)
+# print(type(z))
+
+
+    # 2) Float => Float
+        # A number with decimal point is float
+        # float can be _ve, +ve
+
+# a = 7.5
+# print(a)
+# print(type(a))
+
+# b = 0.0000
+# print(b)
+# print(type(b))
+
+    # 3) Complex => Complex
+        # A combination of Real numbers and Imaginary numbers.
+            # Real:
+                # All Integers and Floats are real numbers.
+            # Imaginary:
+                # A number which comes with 'j' suffix
+
+            # 5+5j => Complex
+# p = 4-8j
+# print(p)
+# print(type(p))
+
+
+    # 4) String
+    # 5) Boolean
+
+    # 6) List
+    # 7) Tuple
+    # 8) Set
+    # 9) Range
+
+    # 10) Dict
